2ee80ff45f6f2c842caea432ae66d444a259820c
vinay kumar yadav <v841241@gmail.com>
2024-04-30 12:19:10 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const apiRoutes = require('./routes/api');
+require('dotenv').config(); 
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect("mongodb+srv://virtualhelp62:Virtual123@cluster0.xgumyjg.mongodb.net/Blackcoffer?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Routes
+app.use('/api', apiRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
